4c52491798ec20cba6c8b2a09139495485b84fa8
Elena <elenaharan@gmail.com>
2024-04-15 07:27:23 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Product from './pages/Product';
+import Homepage from './pages/Homepage';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Homepage />} />
+      <Route path='product' element={<Product />} />
+      <Route path='pricing' element={<Pricing />} />
+      <Route path='*' element={<PageNotFound />} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,9 @@
+function PageNotFound() {
+	return (
+		<h1>
+			Not found :(
+		</h1>
+	)
+}
+
+export default PageNotFound
